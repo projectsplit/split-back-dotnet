fab3d911319ab9cffdacf9d7eae52b3831feb782
thanoskat <thanoskatiras@gmail.com>
2022-12-25 13:34:01 +0200

    Version increment script

---
+++ .github/workflows/scripts/increment-version-dotnet.sh
@@ -0,0 +1,15 @@
+CURRENT_MONTH=$(date +'%y%m')
+CSPROJ_FILE=$1
+VERSION_XML_PATH="Project/PropertyGroup/Version"
+
+LAST_VERSION=$(xmlstarlet select --text --template --value-of $VERSION_XML_PATH $CSPROJ_FILE)
+LAST_VERSION_MONTH=$(echo $LAST_VERSION | cut -d "." -f 1)
+LAST_VERSION_NUMBER=$(echo $LAST_VERSION | cut -d "." -f 2)
+
+if [[ $CURRENT_MONTH -eq $LAST_VERSION_MONTH ]]; then NEW_VERSION=$LAST_VERSION_MONTH.$((LAST_VERSION_NUMBER+1)); fi
+if [[ $CURRENT_MONTH -gt $LAST_VERSION_MONTH ]]; then NEW_VERSION=$CURRENT_MONTH.0; fi
+if [[ $CURRENT_MONTH -lt $LAST_VERSION_MONTH ]]; then exit 1; fi
+
+echo $LAST_VERSION" -> "$NEW_VERSION
+
+xmlstarlet edit --inplace --omit-decl --update $VERSION_XML_PATH -v $NEW_VERSION $CSPROJ_FILE
