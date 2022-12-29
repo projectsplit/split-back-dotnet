07d4bbee08c1444f32063188c227600203ff4532
thanoskat <thanoskatiras@gmail.com>
2022-12-29 21:36:15 +0200

    setup app settings

--- src/SplitBackApi/Program.cs
+++ src/SplitBackApi/Program.cs
@@ -1,30 +1,40 @@
+using Microsoft.Extensions.Options;
+using SplitBackApi.Configuration;
+
 namespace SplitBackApi;
 
 public class Program {
 
   public static void Main(string[] args) {
-    
+
     var builder = WebApplication.CreateBuilder(args);
-    
+
+    builder.Services.Configure<AppSettings>(
+      builder.Configuration.GetSection(AppSettings.SectionName));
+
     // Add services to the container.
     builder.Services.AddAuthorization();
-    
+
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
-    
+
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
-    if(app.Environment.IsDevelopment()) {
+    if (app.Environment.IsDevelopment()) {
       app.UseSwagger();
       app.UseSwaggerUI();
     }
-    
+
     app.UseHttpsRedirection();
-    
+
     app.UseAuthorization();
-    
+
+    app.MapGet("/", (IOptions<AppSettings> appSettings) => {
+      return appSettings.Value;
+    });
+
     app.Run();
   }
 }
