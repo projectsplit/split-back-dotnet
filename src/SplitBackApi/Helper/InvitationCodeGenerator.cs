d78df9489ef9b15a5d423350bc270ff5f1361bf7
thanoskat <thanoskat@gmx.com>
2023-03-01 18:11:13 +0200

    format doc

--- src/SplitBackApi/Helper/InvitationCodeGenerator.cs
+++ src/SplitBackApi/Helper/InvitationCodeGenerator.cs
@@ -2,12 +2,12 @@ using System.Security.Cryptography;
 
 namespace SplitBackApi.Helper;
 
-public static class InvitationCodeGenerator
-{
-  public static string GenerateInvitationCode()
-  {
-    using (var rng = RandomNumberGenerator.Create())
-    {
+public static class InvitationCodeGenerator {
+  
+  public static string GenerateInvitationCode() {
+    
+    using(var rng = RandomNumberGenerator.Create()) {
+      
       byte[] data = new byte[4];
       rng.GetBytes(data);
       uint randomInt = BitConverter.ToUInt32(data, 0);
