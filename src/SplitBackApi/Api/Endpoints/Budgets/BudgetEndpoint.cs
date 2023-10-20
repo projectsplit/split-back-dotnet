51c7a6d5b961340eaa728e726a2fbee7c9c623a7
ChristosKehayas <christos.kechagia@gmail.com>
2023-10-20 22:09:27 +0100

    added spendingInfo endpoint

--- src/SplitBackApi/Api/Endpoints/Budgets/BudgetEndpoint.cs
+++ src/SplitBackApi/Api/Endpoints/Budgets/BudgetEndpoint.cs
@@ -2,15 +2,16 @@ namespace SplitBackApi.Api.Endpoints.Budgets;
 
 public static partial class BudgetsEndpoints
 {
-    public static void MapBudgetEndpoints(this IEndpointRouteBuilder app)
-    {
-        var budgetGroup = app
-            .MapGroup("/budget")
-            .WithTags("Budget")
-            .AllowAnonymous();
+  public static void MapBudgetEndpoints(this IEndpointRouteBuilder app)
+  {
+    var budgetGroup = app
+        .MapGroup("/budget")
+        .WithTags("Budget")
+        .AllowAnonymous();
 
-        budgetGroup.MapPost("/create", CreateBudget);
-        budgetGroup.MapPost("/delete", DeleteBudget);
-        budgetGroup.MapGet("/budgetinfo", GetBudgetInfo);
-    }
+    budgetGroup.MapPost("/create", CreateBudget);
+    budgetGroup.MapPost("/delete", DeleteBudget);
+    budgetGroup.MapGet("/budgetinfo", GetBudgetInfo);
+    budgetGroup.MapGet("/spendinginfo", GetSpendingInfo);
+  }
 }
