9cd8bfab016fd8c88f6c6ef903e579ebe48109bf
ChristosKehayas <christos.kechagia@gmail.com>
2023-10-24 17:47:55 +0100

    validate weekly and monthly budget days

--- src/SplitBackApi/Domain/Validators/BudgetValidator.cs
+++ src/SplitBackApi/Domain/Validators/BudgetValidator.cs
@@ -32,6 +32,19 @@ public class BudgetValidator : AbstractValidator<Budget>
     RuleFor(budget => budget.BudgetType)
     .Must(budgetType => Enum.IsDefined(typeof(BudgetType), budgetType))
     .WithMessage("A valid budget type must be selected");
+
+    RuleFor(budget => budget)
+    .Custom((budget, context) =>
+    {
+      if (budget.BudgetType == BudgetType.Weekly && budget.Day.ToDecimal() > 7)
+      {
+        context.AddFailure("Day cannot be greater than 7 for a weekly budget.");
+      }
+      else if (budget.BudgetType == BudgetType.Monthly && budget.Day.ToDecimal() > 31)
+      {
+        context.AddFailure("Day cannot be greater than 31 for a monthly budget.");
+      }
+    });
   }
 
 }
